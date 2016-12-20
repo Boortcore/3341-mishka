9e79009015382083cf6a54020f15e665f45b1f9b
Sergey Burtylev <deletespaceflashcolonvlan.dat@gmail.com>
2016-12-21 00:30:21 +0300

    Пиксельпёрфект index.html

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,34 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-
-gulp.task("style", function() {
+var mqpacker = require("css-mqpacker");
+var csso = require("gulp-csso");
+var imagemin = require('gulp-imagemin');
+var rename = require("gulp-rename")
+gulp.task("style", function () {
   gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({browsers: [
+      autoprefixer({
+        browsers: [
         "last 2 versions"
-      ]})
+      ]
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    /*.pipe(postcss([
+      mqpacker({
+        sort: true
+      })
+     ]))
+    .
+    .pipe(csso())
+    .pipe(rename("style.min.css"))*/
+  .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", ["style"], function () {
   server.init({
     server: ".",
     notify: false,
@@ -32,3 +45,13 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
+
+
+gulp.task('images', () =>
+  gulp.src('./img/**/*.{png,jpg,gif}')
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+    ]))
+  .pipe(gulp.dest('build/img'))
+);
